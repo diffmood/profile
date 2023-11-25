f866e3ef30506487503215eb2f5c7d6822e43975
elforgaani <mohamed.elforgani1999@gmail.com>
2023-11-25 18:13:55 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Header Text Animation
+
+
+var typed = new Typed("#animated-text", {
+  strings: ["Mohamed", "Developer", "Designer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+  loopCount: Infinity,
+});
